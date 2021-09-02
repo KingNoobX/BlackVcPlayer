7d8c59a7d29369c7f2eb8e8516127be97fae9061
Avnish <89906297+KingNoobX@users.noreply.github.com>
2021-09-02 16:46:14 +0530

    Add files via upload

---
+++ BlackVcPlayer/services/queues/__init__.py
@@ -0,0 +1,7 @@
+from BlackVcPlayer.services.queues.queues import clear 
+from BlackVcPlayer.services.queues.queues import get
+from BlackVcPlayer.services.queues.queues import is_empty
+from BlackVcPlayer.services.queues.queues import put
+from BlackVcPlayer.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
